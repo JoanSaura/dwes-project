6a907f832de184bf227d187f48074b9ef8c0922d
TommyDrug <jsaura20489@iesjoanramis.org>
2024-09-26 20:46:02 +0200

    26.09

---
+++ img/visuals.js
@@ -0,0 +1,10 @@
+const imgBG = document.getElementById("full-site");
+const HiddenMenu = document.getElementById("hidden-menu");
+
+function ChangePicture(){
+
+}
+
+function ShowHiddenMenu() {
+
+}
--- index.php
+++ index.php
@@ -1,11 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hotel Managment System</title>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Hotel Management System</title>
 </head>
+
 <body>
-    
+
+    <div id="full-site">
+
+        <div id="top-container">
+            <div id="head">
+                <img src="" alt="">
+                <h1>Hotel Managment System</h1>
+            </div>
+            <div id="lateral-menu">
+                <a href="">Contacto</a>
+                <a href="">Reserva</a>
+                <a href="">Sobre nosotros</a>
+            </div>
+            <div id="hidden-menu">
+
+            </div>
+        </div>
+
+
+        <div id="main-container">
+            <div id="main-content">
+
+            </div>
+        </div>
+
+        <div id="footer">
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Legal Notice</a>
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Political Privacy</a>
+            <div class="author">
+                <p>Joan Saura © 2024 / 2025</p>
+            </div>
+        </div>
+
+    </div>
 </body>
+
 </html>
