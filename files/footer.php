858f92e74eced13a3c26c8c79a01c9612f8700e1
TommyDrug <jsaura20489@iesjoanramis.org>
2024-10-02 20:47:34 +0200

    2/10

---
+++ files/footer.php
@@ -0,0 +1,12 @@
+<div id="footer">
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Legal Notice</a>
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Political Privacy</a>
+            <div class="author">
+                <p>Joan Saura © 2024 / 2025</p>
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
---
+++ files/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/visuals.js"></script>
+    <title>Hotel Management System</title>
+</head>
+
+<body>
+
+    <div id="full-site">
+
+        <div id="top-container">
+            <div id="head">
+                <img src="" alt="">
+                <h1>MIRACLE Hotel</h1>
+            </div>
+            <div id="lateral-menu">
+                <p href="">Contacto</p>
+                <p onclick="ShowHiddenMenu()">Reserva</p>
+                <p href="">Sobre nosotros</p>
+            </div>
+            <div id="hidden-menu">
+                <a href="">Hacer reserva</a>
+                <a href="">Consultar Reserva</a>
+            </div>
+        </div>
