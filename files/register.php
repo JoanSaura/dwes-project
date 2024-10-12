2bc0436fcc6019b575163b1731461853d8dab3db
Tommy <jsaura20489@iesjoanramis.org>
2024-10-12 20:56:12 +0200

    12.10

--- files/register.php
+++ files/register.php
@@ -1,6 +1,7 @@
 <?php $root = $_SERVER['DOCUMENT_ROOT'];?>
-    <?php include($root . '/student71/dwes/files/common-files/header.php')?>
+<?php include($root . '/student71/dwes/files/common-files/header.php')?>
 
     <?php include($root . '/student71/dwes/files/forms/customers/insert_customer_form.php');?>
-
     <?php include($root . '/student71/dwes/files/common-files/footer.php'); ?>
+
+
