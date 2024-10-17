0952c49e59fd519622197ffb0fa757e5fa96cb46
TommyDrug <jsaura20489@iesjoanramis.org>
2024-10-17 20:41:41 +0200

    17.10

--- files/querys/customers/update_customers.php
+++ files/querys/customers/update_customers.php
@@ -2,11 +2,9 @@
 include($_SERVER['DOCUMENT_ROOT'] . '/student71/dwes/files/common-files/db_connection.php');
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // Asegúrate de que el ID del cliente está presente en el formulario
     if (isset($_POST['client-id'])) {
         $clientId = intval($_POST['client-id']);
         
-        // Asegúrate de que los otros campos están presentes
         if (isset($_POST['first-name']) && isset($_POST['last-name']) && isset($_POST['identification']) && 
             isset($_POST['email']) && isset($_POST['phone'])) {
                 
@@ -16,7 +14,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $email = mysqli_real_escape_string($conn, $_POST['email']);
             $phone = mysqli_real_escape_string($conn, $_POST['phone']);
 
-            // Asegúrate de que la tabla de clientes está correctamente referenciada
             $sqlUpdate = "UPDATE 071_customers SET 
                             client_first_name = '$firstName', 
                             client_last_name = '$lastName', 
@@ -25,7 +22,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                             client_phone_number = '$phone' 
                           WHERE client_id = $clientId";
 
-            // Ejecuta la consulta
             if (mysqli_query($conn, $sqlUpdate)) {
                 echo "Customer updated successfully!";
             } else {
